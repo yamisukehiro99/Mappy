2d07f15344e31ccaf50bd86f4a5c183140e974c6
yami <cursedlawliet@gmail.com>
2022-03-09 14:50:00 +0200

    getting things done

--- script.js
+++ script.js
@@ -9,12 +9,50 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+class Workout {
+  id = (Date.now() + ''). slice(-10);
+  date = new Date();
+  
+  constructor(coords, distance, duration) {
+    this.coords = coords;
+    this.distance = distance; // in km
+    this.duration = duration; // in minutes
+  }
+}
+
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace()
+  }
+  calcPace() {
+    this.pace = this.duration / this.distance;
+    return this.pace
+  }
+}
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+  }
+
+  calcSpeed() {
+    this.speed = this.distance / this.duration / 60;
+    return this.speed;
+  }
+}
+const run1 = new Running([39, -12], 5.2, 24, 178)
+const cyc1 = new Cycling([39, -12], 27, 95, 523)
+
+console.log(run1);
+console.log(cyc1);
 class App {
   #map;
   #mapEvent;
   constructor() {
     this._getPosition();
-    console.log(this.#map);
     //events
     form.addEventListener('submit', this._newWorkout.bind(this));
     //toggling
@@ -60,6 +98,8 @@ class App {
     L.marker(markerCoords).addTo(this.#map).bindPopup(L.popup({maxWidth: 250, minWidth: 100, autoClose: false, closeOnClick: false, className: 'running-popup',}).setContent('Workout')).openPopup(); 
   }
 }
+
+
 
 const app = new App();
 
